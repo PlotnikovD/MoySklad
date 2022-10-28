cf265bab640aa7c6f06758c142ee3a0bde885300
D.Plotnikov <plotnikdima5296@gmail.com>
2022-10-28 20:06:25 +0500

    -create project -create tables with liquibase -create entity

---
+++ src/main/java/com/example/moysklad/MoySkladApplication.java
@@ -0,0 +1,13 @@
+package com.example.moysklad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoySkladApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoySkladApplication.class, args);
+    }
+
+}
